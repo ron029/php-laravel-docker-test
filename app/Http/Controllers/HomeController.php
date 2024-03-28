808ce9befc075c5c18cc874ab807d4f5d8a8e014
ron029 <bonifacioronmark@gmail.com>
2024-03-28 17:36:49 +0800

    define controller for conversion

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\NumberToWordsHelper; // Include the helper class
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -21,8 +23,21 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request, $num = 1234567390)
     {
-        return view('home');
+        $value = $request->session()->get('newNum');
+
+        if ($value) {
+            return view('home.index')->with('words', $value);
+        } else {
+            $words = NumberToWordsHelper::convert($num); 
+            return view('home.index', compact('words'));
+        }
+    }
+
+    public function convert(Request $request)
+    {
+        $words = NumberToWordsHelper::convert($request->input('num'));
+        return response()->json(['words'=> $words, 'num' => $request->input('num')]);
     }
 }
